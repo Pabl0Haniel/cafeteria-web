a9f33e0cd376796a1d450d2129685c05b397c03f
Pablo Haniel <pablo1haniel@gmail.com>
2024-08-28 18:30:46 -0300

    Implementação router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Layout from './templates/Layout';
+import Home from './views/Home';
+import NoPage from './views/NoPage';
+import Cardapio from './views/Cardapio';
+import Login from './views/Login';
 
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -8,8 +14,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: 'cardapio', element: <Cardapio /> },
+      { path: 'login', element: <Login /> },
+      { path: '*', element: <NoPage /> },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
