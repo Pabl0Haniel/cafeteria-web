28a98088d08f633ef129cecc2d573ed099dfc706
Pablo Haniel <pablo1haniel@gmail.com>
2024-09-27 20:30:28 -0300

    Criando Clientes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,5 @@
 import ReactDOM from 'react-dom/client';
-
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
-import Layout from './templates/Layout';
-import Home from './views/Home';
-import NoPage from './views/NoPage';
-import Cardapio from './views/Cardapio';
-import Login from './views/Login';
+import App from './App.jsx';
 
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -13,23 +7,3 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
-import React from 'react';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      { path: '/', element: <Home /> },
-      { path: 'cardapio', element: <Cardapio /> },
-      { path: 'login', element: <Login /> },
-      { path: '*', element: <NoPage /> },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
